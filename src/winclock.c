198a446f929fca6ade532df5244063406f982063
Curtis Whitley <restday7@gmail.com>
2025-03-05 05:04:02 -0500

    getting it to link

--- src/winclock.c
+++ src/winclock.c
@@ -22,9 +22,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "../../agwin/src/agwindefs.h"
+#include "../../agwin/src/agwincore.h"
 
-extern AwApplication* _agwin_ptr_app;
+extern AwAppHeader _agwin_header;
+const AwFcnTable* core;
 
 // The "my..." code and data is used for a simple test, and will
 // be removed once there is a better example of a loaded app.
@@ -37,7 +38,31 @@ AwApplication winclock_app = { "winclock", 0, 0, &winclock_class, NULL, 1 };
 
 int main( void )
 {
-    _agwin_ptr_app = &winclock_app;
+    _agwin_header.app = &winclock_app;
+    core = _agwin_header.core_functions;
+    winclock_class.parent = (*core->get_root_class)();
+
+    AwWindowStyle style;
+    style.border = 1;
+    style.title_bar = 1;
+    style.close_icon = 1;
+    style.minimize_icon = 1;
+    style.maximize_icon = 1;
+    style.menu_icon = 1;
+    style.sizeable = 1;
+    style.moveable = 1;
+
+    AwWindowState state;
+    state.active = 0;
+    state.enabled = 1;
+    state.selected = 0;
+    state.visible = 1;
+
+    AwWindow* win = (*core->create_window)(&winclock_app, NULL, &winclock_class,
+        style, state, 320, 240, 100, 100, "WinClock", 0);
+    win->bg_color = 5;
+    win->fg_color = 7;
+
 	return 0;
 }
 
